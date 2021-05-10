c5e6f0837e39fbb2f3ca6a45f3e39200b4e4d972
kirjai <9858620+kirjai@users.noreply.github.com>
2021-05-10 11:15:57 +0100

    remove everything

--- code/angular2-forms/app/app.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from 'angular2/core';
-import { HeroForm } from './hero-form.component';
-
-@Component({
-    selector: 'my-app',
-    template: '<hero-form></hero-form>',
-    directives: [HeroForm]
-})
-export class AppComponent { }
